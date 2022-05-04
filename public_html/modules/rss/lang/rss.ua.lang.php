0f98e666bd5293b7544f140b4a9b2269528297a1
webitproff <webitproff@gmail.com>
2022-05-04 05:48:14 +0300

    Add files via upload

---
+++ public_html/modules/rss/lang/rss.ua.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Ukrainian Language File for the RSS Module (rss.ua.lang.php)
+ *
+ * @package RSS
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+/**
+ * Module Section
+ */
+
+$L['cfg_rss_timetolive'] = 'Частота оновлення RSS кешу';
+$L['cfg_rss_timetolive_hint'] = '(у секундах)';
+$L['cfg_rss_maxitems'] = 'Макс. кількість елементів в RSS каналі';
+$L['cfg_rss_maxitems_hint'] = '';
+$L['cfg_rss_charset'] = 'Кодування RSS каналів';
+$L['cfg_rss_charset_hint'] = 'Набір символів (кодування сторінки)';
+$L['cfg_rss_pagemaxsymbols'] = 'Макс. кількість символів для сторінок';
+$L['cfg_rss_pagemaxsymbols_hint'] = 'За замовчуванням вимкнено';
+$L['cfg_rss_postmaxsymbols'] = 'Макс. кількість символів для повідомлень форуму';
+$L['cfg_rss_postmaxsymbols_hint'] = 'За замовчуванням вимкнено';
+
+$L['info_desc'] = 'Формує стрічки RSS/Atom для вашого сайту';
+
+/**
+ * Main
+ */
+
+$L['rss_allforums_item_title'] = 'Останні повідомлення на форумі';
+$L['rss_title'] = 'RSS';
+$L['rss_topic_item_desc'] = 'Останні повідомлення в темі';
