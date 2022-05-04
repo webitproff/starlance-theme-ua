b303d79fef9008e74998cf3c50711fc955b6eafa
webitproff <webitproff@gmail.com>
2022-05-04 12:15:29 +0300

    Add files via upload

---
+++ public_html/plugins/ipsearch/lang/ipsearch.ua.lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Ukrainian Language File for IPSearch
+ *
+ * @package IPSearch
+ * @copyright (c) Cotonti Translators Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['info_desc'] = 'Засіб пошуку користувачів по IP-адресі';
+
+/**
+ * Plugin Body
+ */
+
+$L['ipsearch_title'] = 'Пошук по IP';
+$L['ipsearch_dnsrecord'] = 'DNS-запис для даної адреси';
+$L['ipsearch_searchthisuser'] = 'Шукати даний IP в базі користувачів';
+
+$L['ipsearch_found1'] = 'Найдено';
+$L['ipsearch_found2'] = 'співпадінь(ня) для';
+
+$L['ipsearch_help'] = 'Ніякої допомоги для цього плагіна ще нема!';
