4c6de2aee971a2add877b4a9d2914fbb3dcc0dc3
webitproff <webitproff@gmail.com>
2022-05-06 16:09:39 +0300

    Update cwsender.ua.lang.php
    переклад завершено 05_06_2022
    в якості апострофу використовувати символ " ’ "

--- public_html/modules/cwsender/lang/cwsender.ua.lang.php
+++ public_html/modules/cwsender/lang/cwsender.ua.lang.php
@@ -1,5 +1,8 @@
 <?php
-
+/* 
+* переклад українською https://github.com/webitproff 
+* травень 2022
+*/
 defined('COT_CODE') or die('Wrong URL.');
 
 $L['info_desc'] = 'Менеджер розсилок';
@@ -13,42 +16,42 @@ $L['cwsender_lists_form_title'] = 'Заголовок';
 $L['cwsender_lists_form_recipients'] = 'Одержувачі';
 $L['cwsender_lists_form_type_text_title'] = 'Текстовий список';
 $L['cwsender_lists_form_type_text_desc'] = 'Формат списку: E-mail, Ім’я (кожен одержувач в окремому рядку, ім’я вказувати не обов’язково)';
-$L['cwsender_lists_form_type_groups_title'] = 'Группы пользователей';
-$L['cwsender_lists_form_type_groups_desc'] = 'Выберите группы, пользователям которых будет осуществляться рассылка по этому списку.';
-$L['cwsender_lists_form_type_mysql_title'] = 'Mysql-запрос';
-$L['cwsender_lists_form_type_mysql_desc'] = 'Внимание! Только для специалистов. Будьте внимательны при составлении запроса.<br/>Пример запроса: select name as user_name, email as user_email from cot_users where user_maingrp=4</p>';
-$L['cwsender_lists_form_type_subs_title'] = 'Подписка';
-$L['cwsender_lists_form_type_subs_desc'] = 'Для работы данного спика рассылки необходимо разместить форму подписки, разместив в любом шаблоне сайта тэг функции подписки: {PHP|cwsender_subscribe(номер_рассылки)}, где номер_рассылки - это id - списка получателей рассылки.';
+$L['cwsender_lists_form_type_groups_title'] = 'Групи користувачів';
+$L['cwsender_lists_form_type_groups_desc'] = 'Оберіть групи користувачів, членам яких, буде здійснюватися розсилка за цим списком.';
+$L['cwsender_lists_form_type_mysql_title'] = 'Mysql-запит';
+$L['cwsender_lists_form_type_mysql_desc'] = 'Увага! Тільки для спеціалістів. Будьте уважні при складанні запиту.<br/>Приклад запиту: select name as user_name, email as user_email from cot_users where user_maingrp=4</p>';
+$L['cwsender_lists_form_type_subs_title'] = 'Підписка на розсилку';
+$L['cwsender_lists_form_type_subs_desc'] = 'Для роботи даного списку розсилки необхідно розмістити форму підписки, розмістивши в будь-якому шаблоні сайту тег функції підписки: {PHP|cwsender_subscribe(номер_розсилки)}, де номер_розсилки - це є id - списку одержувачів розсилки.';
 
-$L['cwsender_letters_title'] = 'Рассылки';
-$L['cwsender_letters_add_title'] = 'Создание сообщения для рассылки';
-$L['cwsender_letters_add_button'] = 'Создать соощение';
-$L['cwsender_letters_status_ready'] = 'Готово к рассылке';
-$L['cwsender_letters_status_process'] = 'Рассылается...';
-$L['cwsender_letters_status_sent'] = 'Рассылка завершена';
+$L['cwsender_letters_title'] = 'Розсилки повідомлень';
+$L['cwsender_letters_add_title'] = 'Створення меседжу, повідомлення для розсилки';
+$L['cwsender_letters_add_button'] = 'Створити повідомлення';
+$L['cwsender_letters_status_ready'] = 'Готово до здійснення розсилки';
+$L['cwsender_letters_status_process'] = 'Розсилається ...';
+$L['cwsender_letters_status_sent'] = 'Розсилка завершена';
 $L['cwsender_letters_form_title'] = 'Заголовок';
 $L['cwsender_letters_form_text'] = 'Текст';
-$L['cwsender_letters_form_list'] = 'Список рассылки';
-$L['cwsender_letters_unsubs_text'] = ' <br/><br/><a href="{$unsubs_url}">Отписаться от рассылки</a><br/><br/>';
-$L['cwsender_subsuser_title'] = 'Список подписчиков';
+$L['cwsender_letters_form_list'] = 'Список разсилки';
+$L['cwsender_letters_unsubs_text'] = ' <br/><br/><a href="{$unsubs_url}">Відписаться від розсилки</a><br/><br/>';
+$L['cwsender_subsuser_title'] = 'Список підписчиків';
 
-$L['cwsender_error_type_text_empty'] = 'Не заполнен список получателей';
-$L['cwsender_error_type_groups_empty'] = 'Не выбраны группы получателей';
-$L['cwsender_error_type_mysql_empty'] = 'Не составлен mysql-запрос';
+$L['cwsender_error_type_text_empty'] = 'Не заповнено список одержувачів';
+$L['cwsender_error_type_groups_empty'] = 'Не вибрані групи одержувачів';
+$L['cwsender_error_type_mysql_empty'] = 'Не складено mysql-запит';
 
-$L['cwsender_error_letter_title_empty'] = 'Не указан заголовок  рассылки';
-$L['cwsender_error_letter_text_empty'] = 'Не указан текст рассылки';
-$L['cwsender_error_letter_list_notselect'] = 'Не выбран список рассылки';
+$L['cwsender_error_letter_title_empty'] = 'Не вказаний заголовок розсилки';
+$L['cwsender_error_letter_text_empty'] = 'Не заповнено текст розсилки';
+$L['cwsender_error_letter_list_notselect'] = 'Не вибраний список розсилки';
 
-$L['cwsender_subscribe_title'] = 'Подписка на рассылку';
-$L['cwsender_subscribe_name'] = 'Ваше имя';
+$L['cwsender_subscribe_title'] = 'Підписка на розсилку';
+$L['cwsender_subscribe_name'] = 'Ваше им’я';
 $L['cwsender_subscribe_email'] = 'Ваша почта';
-$L['cwsender_subscribe_status_subs_ok'] = 'Поздравляем! Вы подписались на рассылку.';
+$L['cwsender_subscribe_status_subs_ok'] = 'Вітаємо! Ви підписалися на розсилку.';
 
-$L['cwsender_subscribe_error_email_empty'] = 'Не указан email для подписки';
-$L['cwsender_subscribe_error_email_wrong'] = 'Email указан неверно';
-$L['cwsender_subscribe_error_email_exists'] = 'Указанный email уже имеется в списке подписчиков данной рассылки';
+$L['cwsender_subscribe_error_email_empty'] = 'Не вказано email для підписки';
+$L['cwsender_subscribe_error_email_wrong'] = 'Email вказан невірно';
+$L['cwsender_subscribe_error_email_exists'] = 'вказаний email уже існує в списку підписчиків цієї розсилки';
 
-$L['cwsender_unsubscribe_title'] = 'Отписка от рассылки';
-$L['cwsender_unsubscribe_status_unsubs_ok'] = 'Вы отписались от рассылки.';
-$L['cwsender_unsubscribe_status_unsubs_fail'] = 'Не удалось выполнить операцию. Пожалуйста, повторите ваш запрос или обратитесь к администратору сайта.';
+$L['cwsender_unsubscribe_title'] = 'Відмова та відписка від розсилки';
+$L['cwsender_unsubscribe_status_unsubs_ok'] = 'Ви відписалися від розсилки.';
+$L['cwsender_unsubscribe_status_unsubs_fail'] = 'Не вдалося виконати операцію. Будь ласка, повторіть запит або зверніться до адміністратора сайту.';
